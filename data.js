4ba4d3bdaef61bd264ca1e46582f9342e1ddbc91
yanmotion <152085263+yanmotion@users.noreply.github.com>
2023-11-29 23:50:26 +0300

    Commit

---
+++ data.js
@@ -0,0 +1,175 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--1",
+      "name": "Зал 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.06712804815363071,
+        "pitch": -0.049897686874835756,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1615686871261346,
+          "pitch": 0.26208996835522136,
+          "rotation": 0,
+          "target": "1--2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--2",
+      "name": "Зал 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.0026851219261541104,
+        "pitch": -0.04989768687481089,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0022205743761709584,
+          "pitch": 0.15637353109212526,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 1.7085008876819243,
+          "pitch": 0.2461628960807971,
+          "rotation": 0,
+          "target": "0--1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "Примерочная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.0032072341252327163,
+        "pitch": 0.01964494552171203,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05000838052834311,
+          "pitch": 0.4425535426080085,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 3.1043417151434216,
+          "pitch": 0.6432319494519447,
+          "rotation": 0,
+          "target": "1--2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "Касса",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.005063978221013343,
+        "pitch": 0.04477321109574106,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.936820862474967,
+          "pitch": 0.5209516553927109,
+          "rotation": 6.283185307179586,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Madia_Pano",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
